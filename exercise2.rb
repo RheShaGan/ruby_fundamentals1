066b0e6abffadc5dc184986957308f59892421b8
RHEA GANAPATHY <rheasg@gmail.com>
2015-06-09 14:52:30 -0400

    Exercise 2 completed

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+# prints 15% tip on a $55 meal
+puts 55 * 1.15 #how do you round of to 2 decimal places?
+
+# will print person's age
+age = 23
+#converting age value to string
+puts "Rhea is " + age.to_s
+
+#print the answer of 45628 by 7839
+answer = 45628 * 7839
+
+puts "The answer is #{answer}"
+
+#The value of the expression is true
+puts (true && false) || (false && true) || !(false && false)
