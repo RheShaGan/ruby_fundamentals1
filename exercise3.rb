f7fcf1c84866b5b380eb2300b472774319404ed4
RHEA GANAPATHY <rheasg@gmail.com>
2015-06-09 15:12:00 -0400

    Exercise 3 prompts user to enter name and age and calculates which year they wereborn in

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,14 @@
+#prompts user to enter name"
 puts "What is your name?"
+
+name = gets.chomp.capitalize #assigns user's input to variable
+
+puts "Hi #{name}!"
+
+puts "How old are you?"
+
+age = gets.chomp.to_i
+
+year = 2015 - age
+
+puts "You were born in #{year}!"
